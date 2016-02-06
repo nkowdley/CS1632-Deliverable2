8635e8dd88da0330767d4b29847ca9a8e065d579
Neel Kowdley <nkowdley@gmail.com>
2016-02-06 12:23:56 -0500

    add some scanner things

--- src/CoffeeMakerQuest.java
+++ src/CoffeeMakerQuest.java
@@ -1,18 +1,32 @@
-
 /**
- * Write a description of class CoffeeMakerQuest here.
- * 
- * Neel Kowdley <nkowdley@gmail.com>
- * v0.0
- */
+* Write a description of class CoffeeMakerQuest here.
+*
+* @author Neel Kowdley <nkowdley@gmail.com>
+* @version 1.1.0
+*/
+import java.util.*;
+import java.io.*;
+//import java.lang.*;
+
 public class CoffeeMakerQuest
 {
+  public static void main(String[] args) throws Exception
+  {
+    System.out.println("Coffee Maker Quest v1.1");
+    Actions();
+    return;
 
-    public CoffeeMakerQuest()
-    {
-        public static void main(String[] args) throws Exception
-        {
-            System.out.println("Coffee Maker Quest v1.1");
-
-        }
-    }
+  }
+  public static void Actions()
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("INSTRUCTIONS (N,S,L,I,D,H) >" );
+    char input = sc.next(".").charAt(0);
+    System.out.println(input); //Print out input
+    // switch(input)
+    // {
+    //   case "N":
+    // }
+    return;
+  }
+}
