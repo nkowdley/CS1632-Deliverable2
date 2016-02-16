ee879c7a758ee66e9aecf8f33a24b0ffb496537f
Neel Kowdley <nkowdley@gmail.com>
2016-02-15 21:12:07 -0500

    initial junit test class for Room class

---
+++ src/RoomTest.java
@@ -0,0 +1,91 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoomTest {
+
+	@Test
+	public void testRoom() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testPopulateRoom0() {
+		//check that all variables are set properly for each room
+		Room x = new Room(0);
+		assertTrue(x.northDoor);
+		assertFalse(x.southDoor);
+		assertTrue(x.cream);
+		assertFalse(x.coffee);
+		assertFalse(x.sugar);
+	}
+	@Test
+	public void testPopulateRoom1() {
+		//check that all variables are set properly for each room
+		Room x = new Room(1);
+		assertTrue(x.northDoor);
+		assertTrue(x.southDoor);
+		assertFalse(x.cream);
+		assertTrue(x.coffee);
+		assertFalse(x.sugar);
+	}
+	@Test
+	public void testPopulateRoom2() {
+		//check that all variables are set properly for each room
+		Room x = new Room(2);
+		assertTrue(x.northDoor);
+		assertTrue(x.southDoor);
+		assertFalse(x.cream);
+		assertFalse(x.coffee);
+		assertFalse(x.sugar);
+	}
+	@Test
+	public void testPopulateRoom3() {
+		//check that all variables are set properly for each room
+		Room x = new Room(3);
+		assertTrue(x.northDoor);
+		assertTrue(x.southDoor);
+		assertFalse(x.cream);
+		assertFalse(x.coffee);
+		assertFalse(x.sugar);
+	}
+	@Test
+	public void testPopulateRoom4() {
+		//check that all variables are set properly for each room
+		Room x = new Room(4);
+		assertTrue(x.northDoor);
+		assertTrue(x.southDoor);
+		assertFalse(x.cream);
+		assertFalse(x.coffee);
+		assertFalse(x.sugar);
+	}
+	@Test
+	public void testPopulateRoom5() {
+		//check that all variables are set properly for each room
+		Room x = new Room(5);
+		assertFalse(x.northDoor);
+		assertTrue(x.southDoor);
+		assertFalse(x.cream);
+		assertFalse(x.coffee);
+		assertTrue(x.sugar);
+	}
+
+
+
+	
+	@Test
+	public void testGetRoomDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObjDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObjectInRoom() {
+		fail("Not yet implemented");
+	}
+
+}
